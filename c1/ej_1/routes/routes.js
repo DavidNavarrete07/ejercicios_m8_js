724d3fd8e5e8d3e86633aa48df980a4c7766d420
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-08-24 00:55:11 -0400

    Se completa el bonus del ejercicio

--- c1/ej_1/routes/routes.js
+++ c1/ej_1/routes/routes.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { Op } = require('sequelize');
 const fnUtils = require('../functionUtils.js');
 const Autor = require('../models/Autor.js');
 const Libro = require('../models/Libro.js');
 require('../models/asociacion.js');
 
 router.get('/libros', async (req, res) => {
-    const libros = await Libro.findAll();
+    const libros = await Libro.findAll({
+        include: Autor
+    });
     res.render('home.html', { libros });
 });
 
 router.post('/libros', async (req, res) => {
-    const form = await fnUtils.getForm(req);
-    const titulo = form.titulo;
-    const descripcion = form.descripcion;
+    const titulo = req.body.titulo;
+    const descripcion = req.body.descripcion;
     try {
         await Libro.create({ titulo, descripcion });
     } catch (error) {
@@ -22,41 +24,75 @@ router.post('/libros', async (req, res) => {
     res.redirect('http://localhost:3000/api/libros');
 });
 
-router.get('/autores', async (req, res) => {
+router.get('/authors', async (req, res) => {
     const autores = await Autor.findAll();
     res.render('autores.html', { autores });
 });
 
-router.post('/autores', async (req, res) => {
-    const form = await fnUtils.getForm(req);
-    const primerNombre = form.primerNombre;
-    const apellido = form.apellido;
-    const notas = form.notas;
+router.post('/authors', async (req, res) => {
+    const primerNombre = req.body.primerNombre;
+    const apellido = req.body.apellido;
+    const notas = req.body.notas;
     try {
         await Autor.create({ primerNombre, apellido, notas });
     } catch (error) {
         console.log('Error al crear un Autor: ' + error);
     }
-    res.redirect('http://localhost:3000/api/autores');
+    res.redirect('http://localhost:3000/api/authors');
 });
 
-router.get('/libros/ver/:libro_id', async (req, res) => {
+router.get('/books/:libro_id', async (req, res) => {
     const libroId = req.params.libro_id;
-    const libro = await Libro.findOne({ where: { id: libroId } });
-    const autores = await Autor.findAll();
-    res.render('libro.html', { libro, autores });
+    const libro = await Libro.findOne({ include: Autor }, { where: { id: libroId } });
+    let autores = await Autor.findAll({include: Libro});
+    let arregloAutores = [];
+    for (const autor of autores) {
+        for (const libro of autor.libros) {
+            for (const autorLibro of autores) {
+                if(autor.id != autorLibro.id){
+                    arregloAutores.push(autorLibro);
+                }
+            }
+        }
+    }
+    res.render('libro.html', { libro, arregloAutores });
 });
 
-router.get('/autores/ver/:autor_id', async (req, res) => {
+router.get('/authors/:autor_id', async (req, res) => {
     const autorId = req.params.autor_id;
-    const autor = await Autor.findOne({ where: { id: autorId } });
-    const libros = await Libro.findAll();
-    res.render('autor.html', { autor, libros });
+    const autor = await Autor.findOne({ include: Libro }, { where: { id: autorId } });
+    const libros = await Libro.findAll({include: Autor});
+    let arregloLibros = [];
+    for (const libro of libros) {
+        for (const autor of libro.autors) {
+            for (const libroAutor of libros) {
+                if(libro.id != libroAutor.id){
+                    arregloLibros.push(libroAutor);
+                }
+            }
+        }
+    }
+    res.render('autor.html', { autor, arregloLibros });
 });
 
-router.get('/escribir/:libro_id/:autor_id', async (req, res) => {
-    console.log('*****************************************');
-    console.log(req.params);
+// router.get('/escribir/:libro_id/:autor_id', async (req, res) => {
+//     // const autorId = req.params.autor_id;
+//     // const libroId = req.params.libro_id;
+//     // const autor = await Autor.findOne({where: {id: autorId}});
+//     // const libro = await Libro.findOne({where: {id: libroId}});
+//     // const result = await autor.addLibro(libro);
+//     // console.log(result);
+//     console.log(req.params);
+// });
+
+router.post('/escribir', async (req, res) => {
+    const autorId = parseInt(req.body.autorId);
+    const libroId = parseInt(req.body.libroId);
+    const libro = await Libro.findOne({ where: { id: libroId } });
+    const autor = await Autor.findOne({ where: { id: autorId } });
+
+    await libro.addAutor(autor, { through: { selfGranted: false } });
+    // await autor.addLibro([{ titulo: libro.titulo, descripcion: libro.descripcion }]);
 });
 
 
